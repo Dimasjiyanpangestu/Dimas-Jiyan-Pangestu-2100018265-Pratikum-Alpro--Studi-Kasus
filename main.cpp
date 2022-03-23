cd6b4e55efb3f2ea3db4122c24af0f2f9afb534b
DIMAS JIYAN PANGESTU <101696911+Dimasjiyanpangestu@users.noreply.github.com>
2022-03-23 15:54:11 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+//Fadillah Oktaviani - 2100018259
+//Dimas jiyan Pangestu - 2100018265
+//Alfina Amallia - 2100018240
+#include <iostream>
+using namespace std;
+
+int main(){
+	int diskon, i, j, x, tot_bayar;
+	int harga[10], bayar[10], jbrg[10], total = 0;;
+	char nama[30][x];
+	cout<<"==============================="<<endl;
+  	cout<<"========KEDAI AYAM ========"<<endl;
+  	cout<<" Jenis Makanan : "<<endl;
+  	cout<<" 1. Ayam goreng "<<endl;
+  	cout<<" 2. Ayam Bakar "<<endl;
+  	cout<<"===============================\n"<<endl;
+  	
+  	cout<<"Masukkan jumlah pesanan : ";cin>>x;
+  	system("cls");
+  	cout<<"============STRUK PEMBAYARAN=========";
+  	cout<<"========KEDAI AYAM ========"<<endl;
+  	 for(i=0;i<x;i++){
+  		cout<<"==============================="<<endl;
+  		cout<<endl;
+  		cout<< "Masukkan menu ayam    = ";
+  		cin>>nama[i];
+	  	cout<< "Masukkan Harga ayam   = ";
+	 	 cin>>harga[i];
+	  	cout<< "Masukkan Jumlah ayam  = ";
+	  	cin>>jbrg[i];
+	  	bayar[i]=harga[i]*jbrg[i];
+	  	total += bayar[i];
+  		cout<<endl;
+  	}
+  	cout<<" NO   	 	Pesanan    		jumlah			harga "<<endl;
+	for(i=0;i<x;i++){
+ 		cout<<i+1<<"\t\t"<<nama[i]<<"\t\t\t"<<jbrg[i]<<"\t\t\t"<<harga[i]<<endl;
+ 	}
+
+	if (total>45000){
+		diskon = 0.1*total;
+ 		tot_bayar=total-diskon;
+ 	} 
+ 	cout<<"================================================\n";
+ 	cout<<" jumlah bayar 			: Rp. "<<total<<endl;
+ 	if(total > 45000)
+ 	{
+ 		cout<<" Diskon 10%   			: Rp. "<<diskon<<endl;
+ 		cout<<" total bayar jika diskon : Rp. "<<tot_bayar<<endl;
+ 		
+	}
+ 	
+	return 0;
+}
